0be0ca90a591fef6edcb42f0009c8a27c7f7e959
Diego Martínez Sánchez <diegomartinez@tuhabi.mx>
2023-04-17 18:37:00 -0600

    Update gen_tokens.py
    Passed to english the script

--- gen_tokens.py
+++ gen_tokens.py
@@ -1,17 +1,21 @@
-import jwt
-from datetime import datetime, timedelta
+try:
+    import jwt
+    from datetime import datetime, timedelta
+except ImportError as err_imp:
+    print(f"The following import error occurred: {err_imp}")
 
 # Definir la clave secreta para firmar el token
 secret_key = "B7PwGjhYohg"
 
-def encode_token(username: str, days:int=0, minutes:int=120, **kwargs) -> dict[str, str]:
+def encode_token(username: str, days:int=0, minutes:int=120, **kwargs) -> str:
     """
-    Método para crear un JWT por defecto vence en 5min.
+    Method to generate a token for a user with a given expiration time.
 
-    :param username: Nombre de usuario
-    :param days: Cantidad de días que el token será válido
-    :param minutes: Cantidad de minutos que el token será válido
-    :param kwargs: Cualquier otro dato que quieras incluir en el payload del token
+    :param username: Username of the user to generate the token for
+    :param days: Amount of days that the token will be valid
+    :param minutes: Amount of minutes that the token will be valid
+    :param kwargs: Any other data to include in the token payload
+    :return: The generated token
     """
     payload = {
         'exp': datetime.utcnow() + timedelta(days=days, minutes=minutes),
@@ -27,7 +31,7 @@ def encode_token(username: str, days:int=0, minutes:int=120, **kwargs) -> dict[s
 if __name__ == "__main__":
     # Definir los datos a incluir en el payload del token (pueden ser cualquier datos que quieras incluir)
     payload = {
-        "descripcion": "cualquier_cosa"
+        "description": "Test token"
     }
 
     token = encode_token(username='satoshi', days=1, minutes=0, payload=payload)
