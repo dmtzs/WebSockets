781fc1d6465c986f2160e3c983bdf3dde255739f
Diego Martínez Sánchez <diegomartinez@tuhabi.mx>
2023-05-13 11:01:34 -0600

    Update repo
    Now this repo is only for the websocket exactly

--- api/app/__init__.py
+++
@@ -1,8 +0,0 @@
-try:
-    from flask import Flask
-except ImportError as e_imp:
-    print(f"The following import ERROR occurred in {__file__}: {e_imp}")
-
-app = Flask(__name__)
-
-from app import routes, error_handlers
